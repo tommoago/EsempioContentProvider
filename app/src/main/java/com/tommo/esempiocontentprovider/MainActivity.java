eb82eb8ff46ecae3c29deffd34866fc2aa180d67
tommo.ago <tommo.ago@gmail.com>
2020-04-02 12:54:43 +0200

    loader nella main activity

--- app/src/main/java/com/tommo/esempiocontentprovider/MainActivity.java
+++ app/src/main/java/com/tommo/esempiocontentprovider/MainActivity.java
@@ -1,14 +1,49 @@
 package com.tommo.esempiocontentprovider;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.tommo.esempiocontentprovider.adapters.ToDoAdapter;
+import com.tommo.esempiocontentprovider.database.ToDoProvider;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int MY_ID = 1;
+    ListView list;
+    ToDoAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = findViewById(R.id.listToDo);
+        adapter = new ToDoAdapter(this, null);
+        list.setAdapter(adapter);
+        getSupportLoaderManager().initLoader(MY_ID, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, ToDoProvider.TODOS_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        adapter.changeCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        adapter.changeCursor(null);
+
     }
 }
